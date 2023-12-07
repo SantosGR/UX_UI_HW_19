c2a8967628c7253e970510540c7d74ca390ef444
SantosGR <134454475+SantosGR@users.noreply.github.com>
2023-12-07 15:47:57 -0500

    ok

---
+++ js/index.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideDown("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#flip1").click(function(){
+      $("#panel1").slideDown("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#flip2").click(function(){
+      $("#panel2").slideDown("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#flip3").click(function(){
+      $("#panel3").slideDown("slow");
+    });
+  });
+
+    
+    $("li1").hover(function(){
+        $("li1").css("color", "gold");
+     });
+
+     $("li1").mouseout(function(){
+        $("li1").css('color', "white");
+    });
+
+    $("li2").hover(function(){
+        $("li2").css("color", "gold");
+     });
+
+     $("li2").mouseout(function(){
+        $("li2").css('color', "white");
+    });
+
+    $("li3").hover(function(){
+        $("li3").css("color", "gold");
+     });
+
+     $("li3").mouseout(function(){
+        $("li3").css('color', "white");
+    });
+
+    $("li4").hover(function(){
+        $("li4").css("color", "gold");
+     });
+
+     $("li4").mouseout(function(){
+        $("li4").css('color', "white");
+    });
+
+    
